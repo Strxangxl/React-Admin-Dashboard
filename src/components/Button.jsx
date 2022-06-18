737ab468db83965d6e1aa4e8ede83786ea1c2f8b
Strxangxl <chaddharatandeepsingh2345@gmail.com>
2022-06-18 15:31:51 +0530

    Theming done

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
+    return(
+        <button 
+            type="button"
+            style={{ backgroundColor: bgColor,
+                color, borderRadius }}
+            className={`text-${size} p-2
+                hover:drop-shadow-xl`}
+        >
+            {text}
+        </button>
+    )
+}
+
+export default Button;
---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+
+class SparkLine extends React.PureComponent{
+    render(){
+        const { id, type, height, width, color, data, currentColor } = this.props;
+
+        return(
+            <SparklineComponent
+                id={id}
+                height={height}
+                width={width}
+                lineWidth={1}
+                valueType="Numeric"
+                fill={color}
+                border={{ color: currentColor, width: 2 }}
+                tooltipSettings={{
+                    visible: true,
+                    format: '${x} : data ${yval}',
+                    trackLineSettings: {
+                        visible: true,
+                    }
+                }}
+                dataSource={data}
+                xName="x"
+                yName="yval"
+                type={type}
+                >
+                    <Inject services={[SparklineTooltip]} />
+                </SparklineComponent>
+        )
+    }
+}
+
+export default SparkLine;
